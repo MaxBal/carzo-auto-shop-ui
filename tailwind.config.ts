cbea446ef51c1f112b69992605a758d2f40b3c3e
MaxBal <max.baulin15@gmail.com>
2025-07-21 13:49:03 +0300

    Fix tailwind-scrollbar dependency conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,5 +86,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 } satisfies Config;
