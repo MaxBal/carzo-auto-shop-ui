be92de30599452afaf82506840001ec9657c274a
MaxBal <max.baulin15@gmail.com>
2025-08-27 22:36:51 +0300

    Updated App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Index from "./pages/Index";
 import Bags from "./pages/Bags";
 import { CheckoutPage as Checkout } from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
+import { Footer } from "@/components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
           <Route path="/checkout" component={Checkout} />
           <Route component={NotFound} />
         </Switch>
+        <Footer />
       </CartProvider>
     </TooltipProvider>
   </QueryClientProvider>
